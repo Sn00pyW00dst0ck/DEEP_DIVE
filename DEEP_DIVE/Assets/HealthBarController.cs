793f710f5741fe1f4363ad895afd5dd0fca60283
Ryan Porter <58050063+RyPort@users.noreply.github.com>
2023-04-06 19:51:56 -0400

    health bar working

--- DEEP_DIVE/Assets/HealthBarController.cs
+++ DEEP_DIVE/Assets/HealthBarController.cs
@@ -5,31 +5,27 @@ using UnityEngine.UI;
 public class HealthBarController : MonoBehaviour
 {
     public Image healthBar;
-    public CharacterController character;
-    private bool isSubmerged = false;
 
-    // Update is called once per frame
-    void UpdateHealthBar()
+
+    
+    private void OnEnable()
     {
-        healthBar.fillAmount = healthBar.fillAmount - .1f;
-        
+        healthBar.fillAmount = 1;
     }
+
     float elapsed = 0f;
     void Update()
     {
-        if (character.transform.position.y < 113) {
-            Debug.Log("Sumberged");
-            isSubmerged = true;
-        }
-        if (isSubmerged) 
+        
+        
+        
+        elapsed += Time.deltaTime;
+        if (elapsed >= 1f)
         {
-            elapsed += Time.deltaTime;
-            if (elapsed >= 1f)
-            {
-                elapsed = elapsed % 1f;
-                UpdateHealthBar();
-            }
+            elapsed = elapsed % 1f;
+            healthBar.fillAmount = healthBar.fillAmount - .1f;
         }
+        
         
 
     }
--- DEEP_DIVE/Assets/Scripts/UnderwaterEffects.cs
+++ DEEP_DIVE/Assets/Scripts/UnderwaterEffects.cs
@@ -8,16 +8,18 @@ public class UnderwaterEffects : MonoBehaviour
     // Start is called before the first frame update
     [SerializeField]
     GameObject underwaterEffects;
-
+    public GameObject healthBar;
     private void OnTriggerEnter(Collider other)
     {
         RenderSettings.fog = true;
         underwaterEffects.SetActive(true);
+        healthBar.SetActive(true);
     }
 
     private void OnTriggerExit(Collider other)
     {
         RenderSettings.fog = false;
         underwaterEffects.SetActive(false);
+        healthBar.SetActive(false);
     }
 }
