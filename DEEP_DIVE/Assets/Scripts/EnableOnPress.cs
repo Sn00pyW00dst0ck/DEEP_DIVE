80734f4de5ff89462c7c97475fdbd3cf2df59ff3
SweetTea <randomtrooper@protonmail.com>
2023-04-27 00:59:48 -0400

    calibrated hands

---
+++ DEEP_DIVE/Assets/Scripts/EnableOnPress.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class EnableOnPress : MonoBehaviour
+{
+    public InputActionReference menuActivatorReference = null;
+    public GameObject myMenu;
+    private bool toggle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        toggle = false;
+    }
+
+    private void Awake()
+    {
+        menuActivatorReference.action.started += ToggleMenu;
+    }
+
+    private void OnDestroy()
+    {
+        menuActivatorReference.action.started -= ToggleMenu;
+    }
+
+    private void ToggleMenu(InputAction.CallbackContext context)
+    {
+        if (toggle == false)
+        {
+            toggle = true;
+            myMenu.active = true;
+
+        }
+        else
+        {
+            toggle = false;
+            myMenu.active = false;
+        }
+    }
+}
